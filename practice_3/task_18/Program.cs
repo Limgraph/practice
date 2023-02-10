5f69cc1fc34eb7342a752c3323b420f1598bff7d
Limgraph <limgraph@mail.ru>
2023-02-10 23:38:25 +0300

    task_18

---
+++ practice_3/task_18/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.Clear();
+
+Console.Write("Введите номер четверть: ");
+int x = int.Parse(Console.ReadLine()!);
+
+switch (x)
+{
+    case 1:
+        Console.Write("X > 0 || Y > 0");
+        break;
+    case 2:
+        Console.Write("X < 0 || Y > 0");
+        break;
+    case 3:
+        Console.Write("X < 0 || Y < 0");
+        break;
+    case 4:
+        Console.Write("X > 0 || Y < 0");
+        break;
+    default:
+        Console.Write("Вы ввели неправильный номер четверти!");
+        break;
+}
