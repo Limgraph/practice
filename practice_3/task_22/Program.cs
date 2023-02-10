a61dc999ac13efab54dd0bc4667ce511a03344c4
Limgraph <limgraph@mail.ru>
2023-02-11 01:00:28 +0300

    task_22

---
+++ practice_3/task_22/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+Console.Clear();
+
+int count = 1;
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+while (count <= a)
+{
+    Console.Write(count * count);
+    if (count != a)
+    {
+        Console.Write(", ");
+    }
+    count++;
+}
