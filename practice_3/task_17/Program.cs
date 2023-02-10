590ccd0ae86e76aecc654f7b5611e167c0b34b15
Limgraph <limgraph@mail.ru>
2023-02-10 23:18:26 +0300

    task_17

---
+++ practice_3/task_17/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+
+Console.Write("Введите  X: ");
+int X = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите  Y: ");
+int Y = int.Parse(Console.ReadLine()!);
+
+if (Y > 0)
+{
+    if (X > 0)
+    {
+        Console.WriteLine("Первая четверть");
+    }
+    else
+    {
+        Console.WriteLine("Вторая четверть");
+    }
+}
+else
+{
+    if (X < 0)
+    {
+        Console.WriteLine("Третья четверть");
+    }
+    else
+    {
+        Console.WriteLine("Четвёртая четверть");
+    }
+}
