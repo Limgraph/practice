1439f465fdcd19a9da7e6d9754b91884fe5eb4e8
Limgraph <limgraph@mail.ru>
2023-02-12 21:09:20 +0300

    task_37

---
+++ practice_5/task_37/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 37: Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+Console.Clear();
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+int[] MultiArray(int[] inArray)
+{
+    int size = inArray.Length / 2;
+    if(inArray.Length % 2 != 0)
+    {
+        size++;
+    }
+    int[] result = new int[size];
+    for (int i = 0; i < inArray.Length / 2; i++)
+    {
+        result[i] = inArray[i] * inArray[inArray.Length - 1 - i];
+        if(inArray.Length % 2 != 0)
+        {
+            result[size - 1] = inArray[inArray.Length / 2];
+        }
+    }
+    return result;
+}
+
+int[] array = GetArray(5, 0, 10);
+Console.WriteLine(String.Join(", ", array));
+
+int[] multiArray = MultiArray(array);
+Console.WriteLine(String.Join(", ", multiArray));
