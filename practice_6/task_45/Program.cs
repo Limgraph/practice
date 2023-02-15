264e135ad467e2270570937ba61503b08b2f4df6
Limgraph <limgraph@mail.ru>
2023-02-15 22:13:19 +0300

    task_45

---
+++ practice_6/task_45/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+Console.Clear();
+
+int[] CopyArray(int[] inArray)
+{
+    int[] arr = new int[inArray.Length];
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        arr[i] = inArray[i];
+    }
+    return arr;
+}
+
+int[] GetArray(int size, int min_value, int max_value)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(min_value, max_value +1);
+    }
+    return res;
+}
+
+int[] array = GetArray(7, 0, 10);
+Console.WriteLine(String.Join(", ", array));
+
+int[] copyArray = CopyArray(array);
+Console.WriteLine(String.Join(", ", copyArray));
