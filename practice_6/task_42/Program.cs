17857f317cde9ccbc1c1e6555449851407c5f6a5
Limgraph <limgraph@mail.ru>
2023-02-15 21:43:12 +0300

    task_42

---
+++ practice_6/task_42/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+Console.Clear();
+
+string DecToBin(int dec)
+{
+    string result = "";
+    while (dec > 0)
+    {
+        result = (dec % 2).ToString() + result;
+        dec /= 2;
+    }
+    return result;
+}
+Console.WriteLine("Введите число: ");
+int ten = int.Parse(Console.ReadLine()!);
+Console.WriteLine(DecToBin(ten));
