2f46b5a88724f34562d9a2e7030272907ab81864
Limgraph <limgraph@mail.ru>
2023-02-15 21:58:01 +0300

    task_44

---
+++ practice_6/task_44/Program.cs
@@ -0,0 +1,18 @@
+﻿// Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+Console.Clear();
+
+int[] Fib(int n)
+{
+    int[] arr = new int[n];
+    arr[0] = 0;
+    arr[1] = 1;
+    for (int i = 2; i < n; i++)
+    {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+    return arr;
+}
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = Fib(n);
+Console.WriteLine(string.Join(", ", array));
