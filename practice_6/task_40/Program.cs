1574b31bc37904d2749c1bf1bf3678728ee5d6c8
Limgraph <limgraph@mail.ru>
2023-02-15 20:51:28 +0300

    task_40

---
+++ practice_6/task_40/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон
+Console.Clear();
+
+bool Check_three_side(int a, int b, int c)
+{
+    return a + b > c && a + c > b && c + b > a;
+}
+
+Console.WriteLine("Введите первое число: ");
+int x = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int y = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число: ");
+int z = int.Parse(Console.ReadLine()!);
+
+if(Check_three_side(x,y,z))
+{
+    Console.WriteLine("Существует!");
+}
+else
+{
+    Console.WriteLine("Не существует!");
+}
