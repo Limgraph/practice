3f73e5a4baddf59e6d2841f00fe1291e67c5c445
Limgraph <limgraph@mail.ru>
2023-02-21 22:48:57 +0300

    task_49

---
+++ practice_7/task_49/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+
+Console.Clear();
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");//'\t' - табуляция; \n перенос на следующую строку; Escape последовательность, управляющая последовательность
+        }
+        Console.WriteLine();
+    }
+}
+
+void ConvertArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i += 2)
+    {
+        for (int j = 0; j < arr.GetLength(1); j += 2)
+        {
+            arr[i, j] *= arr[i, j];
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите количество строк: ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов: ");
+int col = int.Parse(Console.ReadLine()!);
+
+int[,] array2D = GetArray(row, col, 1, 10);
+PrintArray(array2D);
+ConvertArray(array2D);
+PrintArray(array2D);
