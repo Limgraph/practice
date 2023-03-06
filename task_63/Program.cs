b39d3e52ada8b900b5e2ad9da13105e2f67e0800
Limgraph <limgraph@mail.ru>
2023-03-06 19:25:15 +0300

    practice_9

---
+++ task_63/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+Console.Clear();
+
+string Recursion(int n)
+{
+    if (n == 1) return n.ToString();
+    else
+    {
+        return Recursion(n - 1) + ", " + n.ToString();
+    }
+}
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Recursion(n));
---
+++ task_67/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+Console.Clear();
+
+int Numbers(int n)
+{
+    if (n == 0) return 0;
+    else return n % 10 + Numbers(n / 10);
+}
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Numbers(n));
